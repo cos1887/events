1fe9fe0bdb88120076c260273f312710639ac128
Pedro Cos <pedro.cos@accedo.tv>
2020-10-03 23:14:40 +0200

    feat(db): new name for the database

--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ const app = require('./app');
 const port = 3001; 
 
 
-//mongoose.connect ('mongodb://localhost:27107/example')
-
-mongoose.connect('mongodb://localhost:27017/eventos', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/events_db', {useNewUrlParser: true})
     .then(() => {
         app.listen(port, function () {
             console.log("server running at port", port); 
